deb5e3efc4d0346643e5bb8df792a827c4430612
NatalyDid <nat.khom@ukr.net>
2018-07-23 20:40:10 +0300

    project added

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+var gulp = require('gulp'),
+    pug = require('gulp-pug'),
+    sass = require('gulp-sass'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    cache = require('gulp-cache'),
+    watch = require('gulp-watch'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync').create();
+
+
+/*gulp.task('pug', function () {
+    gulp.src('./pug/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .on('error', function (err) {
+            console.log(err);
+        })
+        .pipe(gulp.dest('./app'))
+});*/
+
+gulp.task('img', function () {
+    return gulp.src('app/img/**/*')
+        .pipe(cache(imagemin({
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        })))
+        .pipe(gulp.dest('app/img'));
+});
+
+gulp.task('sass', function () {
+    gulp.src('./scss/**/*.scss')
+        .pipe(sass())
+        .on('error', function (err) {
+            console.log(err);
+        })
+        .pipe(autoprefixer({browsers: ['last 15 versions'], cascade: false}))
+        .pipe(gulp.dest('app/css'));
+});
+
+
+
+gulp.task('watch', function () {
+    gulp.watch('scss/*.scss', ['sass']); 
+ //   gulp.watch('pug/*.pug', ['pug']);
+ //    gulp.watch('pug/**/*.pug').on('change', function () {
+ //       setTimeout(function () {
+ //           browserSync.reload();
+ //       }, 1000)
+ //   });
+    gulp.watch('scss/*.scss').on('change', function () {
+        setTimeout(function () {
+            browserSync.reload();
+        }, 1000)
+    });
+    gulp.watch('app/js/*.js').on('change', browserSync.reload);
+    gulp.watch('app/**/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./app/"
+        }
+    });
+});
+
+gulp.task('default', [/*'pug', */'sass','watch', 'browser-sync']);
